36fc2a2596b9c12954cf2f501c1c5d3fa9b1a9bb
Sakthi Ananthan Amirtharaj <sakthi.ananthan.amirtharaj@oracle.com>
2022-05-22 23:58:24 +0530

    adding app features

--- my_app/application/app.py
+++ my_app/application/app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('hompage.html')
+
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    return render_template('welcome.html', first_name=first_name,last_name=last_name)
+    # return f'Hello {first_name} {last_name} have fun learning python <br/> <a href="/">Back Home</a>'
