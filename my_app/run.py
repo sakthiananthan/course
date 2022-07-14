1e398c63da269fb2aaff5f723bbca4132c9b98f6
Ubuntu <ubuntu@ip-172-31-33-80.ap-south-1.compute.internal>
2022-07-14 17:09:21 +0000

    making the code runnable in server

--- my_app/run.py
+++ my_app/run.py
@@ -3,4 +3,4 @@ from application.app import app
 
 if __name__ == '__main__':	
 
-	app.run(host='127.0.0.1', port=5000, debug=True) #run the app
+	app.run(host='0.0.0.0', port=5000, debug=True) #run the app
